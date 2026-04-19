1d6dc9e11bfcb27c5a45bad97bcb9c2b8404b50c
Ubuntu <ubuntu@localhost.localdomain>
2026-04-19 21:57:12 +0800

    Add shmem register tiling benchmark

--- labs/tiled_conv/main.cu
+++ labs/tiled_conv/main.cu
@@ -140,7 +140,7 @@ static int eval(const shape wDims, const shape xDims, bool doVerify, ConvAlgorit
 }
 
 TEST_CASE("Convlayer", "[convlayer]") {
-  ConvAlgorithm algorithm = ConvAlgorithm::MatmulConceptualUnrollingRegisterTiled;
+  ConvAlgorithm algorithm = ConvAlgorithm::MatmulConceptualUnrollingShmemRegisterTiled;
 #if 0
   SECTION("[wDims:32,1,5,5 xDims:20,1,28,28]") {
     eval({32, 1, 5, 5}, {20, 1, 28, 28}, true, algorithm);
